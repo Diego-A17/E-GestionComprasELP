7921544935951f2b889ad1c04235b892dae58d8b
Diego-A17 <diego.aliaga.g17@gmail.com>
2026-08-04 16:27:19 -0500

    Base inicial del proyecto

---
+++ src/main/java/dag/ECGestionCompras/EcGestionComprasApplication.java
@@ -0,0 +1,13 @@
+package dag.ECGestionCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcGestionComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcGestionComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dag/ECGestionCompras/EcGestionComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package dag.ECGestionCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcGestionComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
